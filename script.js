87edeed5d11059ac19afbf96f60659d5de71c1e6
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-05 14:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validEmail(str) {
   //your JS code here.
  const emailPattern = /^(?!.*[.-]{2})[\w.-]+@[a-zA-Z_]+(?:\.[a-zA-Z]{2,3})+$/;
-    return emailPattern.test(email);
+    return emailPattern.test(str);
+
 	
 	
 }
