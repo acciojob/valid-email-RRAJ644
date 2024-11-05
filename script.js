570b8a95978aa9d00f920467d679f8940d4a94a1
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-05 14:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+ const emailPattern = /^(?!.*[.-]{2})[\w.-]+@[a-zA-Z_]+(?:\.[a-zA-Z]{2,3})+$/;
+    return emailPattern.test(email);
+	
+	
 }
 
 // Do not change the code below.
